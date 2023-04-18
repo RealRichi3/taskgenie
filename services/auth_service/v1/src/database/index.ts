f182948f1b9de8c0a9fe8eb2ba1dfb89be9fbe8b
Richie <molunorichie@gmail.com>
2023-04-18 17:49:33 +0100

    add redis setup

--- services/auth_service/v1/src/database/index.ts
+++ services/auth_service/v1/src/database/index.ts
@@ -1,6 +1,7 @@
 import Mongoose from 'mongoose';
 import { NodeENV } from '../types';
 import * as config from '../config';
+import redis, { RedisClientType} from 'redis';
 
 function getDBConnectionString(env: NodeENV): string {
     switch (env) {
@@ -15,11 +16,39 @@ function getDBConnectionString(env: NodeENV): string {
     }
 }
 
-export async function connectToDatabase() {
+async function initRedisConnection() {
+    const redis_url: string = config.REDIS_URI;
+
+    const redis_client: RedisClientType = redis.createClient({
+        socket: {
+            host: config.REDIS_HOST,
+            port: config.REDIS_PORT,
+        },
+        username: config.REDIS_USERNAME,
+        password: config.REDIS_PASSWORD,
+    });
+
+    redis_client.on('connect', () => {
+        console.log(`Connection to REDIS database successful`);
+    });
+
+    redis_client.on('error', (err: Error) => {
+        console.log(`Error connecting to REDIS database: ${err}`);
+    });
+}
+
+async function initMongoDBConnection() {
     const mongo_url: string = getDBConnectionString(config.NODE_ENV);
 
     Mongoose.set('strictQuery', false);
     await Mongoose.connect(mongo_url);
 
-    console.log(`Connection to ${Mongoose.connection.name} database successful`);
+    console.log(`Connection to ${Mongoose.connection.name} MongoDB database successful`);
+}
+
+export async function connectToDatabase() {
+    await initMongoDBConnection();
+    await initRedisConnection();
+
+    return Mongoose.connection;
 }
