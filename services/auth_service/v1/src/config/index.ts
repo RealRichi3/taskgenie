a7481728d823bfdb5529f198a712950b45be6e6a
Richie <molunorichie@gmail.com>
2023-04-18 21:59:02 +0100

    remove unused variables

--- services/auth_service/v1/src/config/index.ts
+++ services/auth_service/v1/src/config/index.ts
@@ -4,10 +4,6 @@ export const MONGO_URI: string = process.env.MONGO_URI as string,
     MONGO_URI_TEST: string = process.env.MONGO_URI_TEST as string,
     MONGO_URI_DEV: string = process.env.MONGO_URI_DEV as string,
     MONGO_URI_PROD: string = process.env.MONGO_URI_PROD as string,
-    REDIS_USERNAME: string = process.env.REDIS_USERNAME as string,
-    REDIS_PASSWORD: string = process.env.REDIS_PASSWORD as string,
-    REDIS_HOST: string = process.env.REDIS_HOST as string,
-    REDIS_PORT: number = parseInt(process.env.REDIS_PORT as string, 10) || 6379,
     REDIS_URL: string = process.env.REDIS_URL as string;
 
 export const PORT: number = parseInt(process.env.PORT as string, 10) || 5555;
