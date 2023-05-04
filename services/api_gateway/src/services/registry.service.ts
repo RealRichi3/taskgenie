c710e41892306418b3eb300d852cc7f3718b610b
Richie <molunorichie@gmail.com>
2023-05-05 00:33:56 +0100

    add: service controls

--- services/api_gateway/src/services/registry.service.ts
+++ services/api_gateway/src/services/registry.service.ts
@@ -3,6 +3,7 @@ import { Request } from 'express'
 import { APIService, IInstanceDoc, IServiceDoc, Registry } from '../types'
 import { Instance, Service } from '../models'
 import wrapper from '../middlewares/async_wrapper'
+import mongoose from 'mongoose'
 
 type ServiceData = {
     name: string;
@@ -18,7 +19,7 @@ async function addServiceToRegistry(service: ServiceData, req: Request) {
         let new_instance: IInstanceDoc | undefined
         let existing_instance: IInstanceDoc | undefined | null
         const existing_service = await Service.findOne({ name: service.name })
-    
+
         // Check if service has been registered before
         if (!existing_service) {
             // Service hasn't been registered, register and create instance
@@ -34,35 +35,85 @@ async function addServiceToRegistry(service: ServiceData, req: Request) {
                     host: service.host,
                     port: service.port
                 })
-    
+
             if (!existing_instance) {
-                new_instance = await Instance.create({ ...service, service: existing_service._id})
+                new_instance = await Instance.create({ ...service, service: existing_service._id })
             }
         }
-        
+
         console.log(existing_service)
         console.log(new_service)
         return new_instance || existing_instance
     } catch (error) {
         console.log(error)
-        throw error        
+        throw error
     }
 }
 
-async function removeServiceFromRegistry() { }
+async function removeServiceFromRegistry(service_id: mongoose.Types.ObjectId) {
+    try {
+        const existing_service = await Service.findById(service_id)
 
-async function getServicesFromRegistry() { }
+        if (!existing_service) {
+            throw new Error('Service not found')
+        }
 
-async function getServiceFromRegistry() { }
+        await existing_service.deleteOne()
 
-async function checkIfServiceIsRegistered() { }
+        // Delete all instances too
+
+        await Instance.deleteMany({ service: service_id })
+
+    } catch (error) {
+        throw error
+    }
+}
+
+async function unregisterInstance(instance_id: mongoose.Types.ObjectId) {
+    try {
+        const existing_instance = await Instance.findById(instance_id)
+
+        if (!existing_instance) {
+            throw new Error('Instance not found')
+        }
+
+        await existing_instance.deleteOne()
+    } catch (error) {
+        throw error
+    }
+}
+
+async function getServicesFromRegistry() {
+    try {
+        const services = await Service.find()
+
+        return services
+    } catch (error) {
+        throw error
+    }
+ }
+
+async function getServiceFromRegistry(service_id: mongoose.Types.ObjectId) { 
+    try {
+        const existing_service = await Service.findById(service_id)
+
+        if (!existing_service) {
+            throw new Error('Service not found')
+        }
+
+        return existing_service
+    } catch (error) {
+        throw error   
+    }
+}
+
 
 export {
     addServiceToRegistry,
     removeServiceFromRegistry,
     getServicesFromRegistry,
     getServiceFromRegistry,
-    checkIfServiceIsRegistered
+    unregisterInstance
 }
 
 export default {
@@ -70,6 +121,6 @@ export default {
     removeServiceFromRegistry,
     getServicesFromRegistry,
     getServiceFromRegistry,
-    checkIfServiceIsRegistered
+    unregisterInstance
 }
 
